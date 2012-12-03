fee2b8075e262027b485fb1682610d49534a41c0
Mark Burton <mark@greensocs.com>
2012-12-03 21:43:19 +0100

    check last sshd activity time

--- files/default/autoshutdown.sh
+++ files/default/autoshutdown.sh
@@ -7,11 +7,12 @@ LAST=$(date -d "$LASTAUTH" '+%s')
 TENOLD=$(date -d '10 minutes ago' '+%s')
 RUNNING=$(ps aux | awk '{print $1}' | grep vagrant | wc -l)
 
-echo "LAST $LAST OLD $TENOLD RUNNING $RUNNING" >> /vagrant/shutdown.log
+#echo "LAST $LAST OLD $TENOLD RUNNING $RUNNING" >> /vagrant/shutdown.log
 
 if (( TENOLD > LAST )) ; then 
   if (( RUNNING == 0 )) ; then 
-    echo "SHUDOWN!" >> /vagrant/shutdown.log
+      shutdown -P
+#    echo "SHUDOWN!" >> /vagrant/shutdown.log
   fi
 fi
 
