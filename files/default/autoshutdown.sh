92c93a54625fcb8f763b0a0be51430c488f50ea3
Mark Burton <mark@greensocs.com>
2012-12-03 21:10:36 +0100

    check last ssh authentification time

--- files/default/autoshutdown.sh
+++ files/default/autoshutdown.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-if [ `ps  aux | awk '{print $1}' | grep vagrant | wc -l` -gt 0 ] ; then
-  shutdown -c >& /dev/null &
+
+LAST=date -d "`sudo grep "Accepted publickey for vagrant" /var/log/auth.log | tail -1 | awk '{print $1" "$2" "$3}'`" '+%s'
+OLD=date -d '10 minutes ago' '+%s'
+
+if [ OLD < LAST ] ; then 
+	shutdown -c >& /dev/null &
 else
-  shutdown -P +10 >& /dev/null &
+    if [ `ps  aux | awk '{print $1}' | grep vagrant | wc -l` -gt 0 ] ; then
+	shutdown -c >& /dev/null &
+    else
+	shutdown -P +10 >& /dev/null &
+    fi
 fi
 
