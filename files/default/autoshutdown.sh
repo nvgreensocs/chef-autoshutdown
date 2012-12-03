ff2e6d2bcc80287d2a8097dd823a1bcc66a35411
Mark Burton <mark@greensocs.com>
2012-12-03 15:45:49 +0100

    Initial recipie for auto shutdown

---
+++ files/default/autoshutdown.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ `ps  au | awk '{print $1}' | grep vagrant | wc -l` -gt 0 ] ; then
+  shutdown -c >& /dev/null
+else
+  shutdown -P +10 >& /dev/null
+fi
+
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,1 +1,22 @@
-package ""
+
+#  -------    CHEF-AUTOSHUTDOWN --------
+
+# LICENSETEXT
+# 
+#   Copyright (C) 2012 : GreenSocs Ltd
+#       http://www.greensocs.com/ , email: info@greensocs.com
+# 
+# The contents of this file are subject to the licensing terms specified
+# in the file LICENSE. Please consult this file for restrictions and
+# limitations that may apply.
+# 
+# ENDLICENSETEXT
+
+
+cookbook_file /etc/cron.d/autoshutdown do
+  source "autoshutdown"
+end
+
+cookbook_file /usr/local/bin/autoshutdown.sh do
+  source "autoshutdown.sh"
+end
