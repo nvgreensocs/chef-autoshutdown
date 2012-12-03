2949059c078ab9ef1a454a88a8d5384da5250905
Mark Burton <mark@greensocs.com>
2012-12-03 15:47:07 +0100

    Initial recipie for auto shutdown

--- files/default/autoshutdown.sh
+++ files/default/autoshutdown.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [ `ps  au | awk '{print $1}' | grep vagrant | wc -l` -gt 0 ] ; then
-  shutdown -c >& /dev/null
+  shutdown -c >& /dev/null &
 else
-  shutdown -P +10 >& /dev/null
+  shutdown -P +10 >& /dev/null &
 fi
 
