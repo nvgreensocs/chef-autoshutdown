9b10829bac2dbbd748acf9182e8f8d5672ccd5f7
Mark Burton <mark@greensocs.com>
2012-12-03 21:38:36 +0100

    check last sshd activity time

--- files/default/autoshutdown.sh
+++ files/default/autoshutdown.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 
-LAST=`date -d "`sudo grep "Accepted publickey for vagrant" /var/log/auth.log | tail -1 | awk '{print $1" "$2" "$3}'`" '+%s'`
-OLD=`date -d '10 minutes ago' '+%s'`
+# ANY sshd activity is indication that the machine was busy.
+LASTAUTH=`grep "sshd" /var/log/auth.log | tail -1 | awk '{print $1" "$2" "$3}'`
+LAST=$(date -d "$LASTAUTH" '+%s')
+TENOLD=$(date -d '10 minutes ago' '+%s')
+RUNNING=$(ps aux | awk '{print $1}' | grep vagrant | wc -l)
 
-if [ OLD > LAST ] ; then 
-    if [ `ps  aux | awk '{print $1}' | grep vagrant | wc -l` -eq 0 ] ; then
-	shutdown -P
-    fi
+echo "LAST $LAST OLD $TENOLD RUNNING $RUNNING" >> /vagrant/shutdown.log
+
+if (( TENOLD > LAST )) ; then 
+  if (( RUNNING == 0 )) ; then 
+    echo "SHUDOWN!" >> /vagrant/shutdown.log
+  fi
 fi
 
