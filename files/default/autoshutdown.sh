3828aed843847a754088bbcadf84976edadab842
Mark Burton <mark@greensocs.com>
2012-12-03 20:28:23 +0100

    fix ps

--- files/default/autoshutdown.sh
+++ files/default/autoshutdown.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ `ps  au | awk '{print $1}' | grep vagrant | wc -l` -gt 0 ] ; then
+if [ `ps  aux | awk '{print $1}' | grep vagrant | wc -l` -gt 0 ] ; then
   shutdown -c >& /dev/null &
 else
   shutdown -P +10 >& /dev/null &
